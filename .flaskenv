91ac0333f4d2943f629061d2eba92e8bdb82355c
Neale Johnston <neale@team-johnston.com>
2020-11-06 16:16:44 +0000

    Initial folders created, DB Created

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=gym.py
+FLASK_ENV=development
---
+++ app/db/gym.sql
@@ -0,0 +1,42 @@
+DROP TABLE activities;
+DROP TABLE members;
+DROP TABLE membership_types;
+DROP TABLE instructors;
+
+
+CREATE TABLE instructors (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    address TEXT,
+    phone_number VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL
+);
+
+CREATE TABLE membership_types (
+    id SERIAL PRIMARY KEY,
+    typed VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    address TEXT,
+    phone_number VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    membership_type INT REFERENCES membership_types(id),
+    start_date DATE NOT NULL,
+    active_membership BOOLEAN NOT NULL
+);
+
+CREATE TABLE activities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    instructor INT REFERENCES instructors(id),
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    duration INT NOT NULL,
+    capacity INT NOT NULL,
+    membership_type INT REFERENCES membership_types(id)
+);
